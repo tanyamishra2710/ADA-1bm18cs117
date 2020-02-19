26b7452aac7b5857b387e57ac06b9e0d05393ebf
tanyamishra2710 <53753782+tanyamishra2710@users.noreply.github.com>
2020-02-19 12:52:07 +0530

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,47 +1,46 @@
 #include<stdio.h>
-void selection(int a[],int n,int k)
+#include<time.h>
+
+void selection(int a[],int n)
 {
+clock_t start, end;
+double time_used;
 int min,i,j,t;
+start = clock();
 for(i=0;i<n-2;i++)
 {
 min=i;
 for(j=i+1;j<n;j++)
 {
-if(a[j]>a[min])
+if(a[j]<a[min])
 min=j;
 }
 t=a[i];
 a[i]=a[min];
 a[min]=t;
 }
-printf("k largest numbers:\n");
-for(i=0;i<k;i++)
+end = clock();
+     time_used = ((double)(end - start))/ CLOCKS_PER_SEC;
+     time_used=1000000*time_used;
+     printf("TIME TAKEN IN MILLISECONDS: %f\n",time_used);
+printf("Array in sorted manner:\n");
+for(i=0;i<n;i++)
 {
 printf("%d  ",a[i]);
 }
 }
-void largest(int a[],int n,int k)
-{
-int i,j,r,l,num,count=0;
-l=0;
-int new[10];
-selection(a,n,k);
-}
-
-
-
-
 void main()
 {
 int l,i,key;
 int arr[10];
+
 printf("Enter length of array\n");
 scanf("%d",&l);
 printf("Enter array elements\n");
 for(i=0;i<l;i++)
 scanf("%d",&arr[i]);
-printf("enter value of k\n");
-scanf("%d",&key);
-largest(arr,l,key);
+
+
+selection(arr,l);
 
 }
